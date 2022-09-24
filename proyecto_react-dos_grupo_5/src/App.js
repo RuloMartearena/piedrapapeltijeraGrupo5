5216bd7f3470d640efb3b8ef17a4e25458a4bc51
RuloMartearena <93354080+RuloMartearena@users.noreply.github.com>
2022-09-24 20:43:27 -0300

    Fix: fix rules page. Matías Martearena

--- proyecto_react-dos_grupo_5/src/App.js
+++ proyecto_react-dos_grupo_5/src/App.js
@@ -1,21 +1,9 @@
-import './App.css';
-import Reglas from './Components/Reglas';
-import reglas from './Json/reglas.json';
-
+import Reglas from './components/Reglas';
 
 function App() {
   return (
     <>
-    <Reglas/>
-    {reglas.map(reglamento =>
-    <Reglas
-     line0={reglamento.line0}
-     line1={reglamento.line1}
-     line2={reglamento.line2}
-     line3={reglamento.line3}
-     line4={reglamento.line4}
-    ></Reglas>)
-  }
+      <Reglas />
     </>
   );
 }
--- proyecto_react-dos_grupo_5/src/Components/Reglas.js
+++ proyecto_react-dos_grupo_5/src/Components/Reglas.js
@@ -1,16 +1,17 @@
-import '../Css/reglas.css';
-function Reglas(props){
+function Reglas() {
     return (
-        <body>
-            <div className="colorLetra">
-                <h2>{props.line0}</h2> 
-                <h2>{props.line1}</h2>
-                <h2>{props.line2}</h2>
-                <h2>{props.line3}</h2>
-                <h2>{props.line4}</h2>
+        <body className="body__rules">
+            <div className="div__rules-conteiner">
+                <h2>REGLAS</h2>
+                <div>
+                    <p>1. Presta atención al resultado que al darle click a un boton, el juego a comenzado.</p>
+                    <p>2. Elige sabiamente. ¡PIEDRA, PAPEL, TIJERAS! La maquina te espera.</p>
+                    <p>3. 1 punto al ganador, 0 al perdedor, mejor ganar que caer en desesperación.</p>
+                    <p>4. ¿Llegarás a la meta? 5 puntos, que nada te detenga.</p>
+                    <p>5. Ganaste la batalla pero no la guerra, juega a tus anchas para dar una revancha.</p>
+                </div>
             </div>
         </body>
-        
     );
 }
 export default Reglas;

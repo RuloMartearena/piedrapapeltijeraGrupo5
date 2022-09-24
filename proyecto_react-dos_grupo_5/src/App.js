5ce4cfdf4c91e338da08a43ad7bbaa11f63993b1
RuloMartearena <93354080+RuloMartearena@users.noreply.github.com>
2022-09-24 20:00:50 -0300

    Fix: developers page fix. Matías Martearena

--- proyecto_react-dos_grupo_5/src/App.js
+++ proyecto_react-dos_grupo_5/src/App.js
@@ -1,23 +1,10 @@
-import './App.css';
 import Desarroladores from './components/desarroladores';
-import desarroladore from './components/desarroladore.json';
-
 
 function App() {
   return (
     <>
-      
-          {desarroladore.map (desa =>
-            <Desarroladores
-            title={desa.title}
-            name1={desa.name1}
-            name2={desa.name2}
-            name3={desa.name3}
-            name4={desa.name4}
-            name5={desa.name5}
-            name6={desa.name6}
-            grupo={desa.grupo}
-          ></Desarroladores>)}
+      <Desarroladores />
+    </>
   );
 }
 

ce63c243f4bf3e052ceb6a2d9c18fe253ca90c55
RuloMartearena <93354080+RuloMartearena@users.noreply.github.com>
2022-09-25 16:33:39 -0300

    Fix: fix remembers page. Matías Martearena

--- proyecto_react-dos_grupo_5/src/App.js
+++ proyecto_react-dos_grupo_5/src/App.js
@@ -1,19 +1,9 @@
-import './App.css';
-import Reglas from './components/Reglas';
-import reglas from './json/reglas.json';
-import './index.css'
+import Recordatorio from './components/Recordatorio';
 
 function App() {
   return (
     <>
-    {reglas.map(record =>
-      <Reglas
-        title={record.title}
-        line1={record.line1}
-        line2={record.line2}
-        line3={record.line3}
-        ></Reglas>)}
-      
+    <Recordatorio />
     </>
   );
 }
